b093ca20364cfb4ec308a0484237d6d15c1f219b
Alberto Silva <albertossilva@gmail.com>
2018-04-18 16:20:23 -0300

    Create new function

--- lambda.js
+++ lambda.js
@@ -2,4 +2,5 @@ require('dotenv').config()
 
 exports.execGraphql = require('./Infra/lambdas/execGraphql')
 exports.sqsToGraphql = require('./Infra/lambdas/sqsToGraphql')
+exports.kinesisToGraphql = require('./Infra/lambdas/kinesisToGraphql')
 exports.sendMsgGraphql = require('./Infra/lambdas/sendMsgGraphql')
